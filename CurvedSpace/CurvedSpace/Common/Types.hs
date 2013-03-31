486b94eb59f0e7659450d95bcbd11c1780c4a5bf
Alexandr Granin <graninas@gmail.com>
2013-04-01 01:02:39 +0700

    - Designing istead of developing. - WorkflowManager design started. - Added corresponded modules organized in new structure. - Old code moved to 'Trash'.

--- CurvedSpace/CurvedSpace/Common/Types.hs
+++
@@ -1,9 +0,0 @@
-module Common.Types where
-
-import qualified System.CPUTime as T
-import qualified Control.Monad.State as State
-import qualified Data.Word
-
-type Word = Data.Word.Word64
-
-
--- CurvedSpace/CurvedSpace/CurvedSpace.hs
+++ CurvedSpace/CurvedSpace/CurvedSpace.hs
@@ -1,92 +1,15 @@
 module Main where
 
-import qualified Control.Concurrent.MVar as M (tryTakeMVar, tryPutMVar, newEmptyMVar, newMVar, MVar) 
-import qualified Control.Concurrent as C
-import qualified Graphics.UI.GLUT as GLUT
-import qualified Control.Monad.State as State
-import Control.Monad (when)
-import Data.Maybe (isJust, fromJust)
+import qualified Workflow.WorkflowFacade as W
 
-import Common.Types
-import Common.InteropTypes
-import Common.GLTypes
-import Common.GL
-import Common.Constants
-import Common.TickTime
-import Draw.Draw
-
-import Common.World
-
- 
-data GameWorld = GameWorld
-     { gwObjectPos :: WorldDim
-     , gwObjectVelocity :: (Word, Word, Word)
-     }
-  deriving (Show, Eq)
-
-type GW a = State.StateT GameWorld IO a
 
 main::IO ()
 main = do
 
-    gameMVar <- M.newEmptyMVar
-    worldMVar <- M.newEmptyMVar
-    let mvars = (gameMVar, worldMVar)
-
-    wnd <- initializeGLWindow drawScene
-
--- Main game loop
-    currentTime <- getIOTickTime
-    let nextTactTime = addTickTime tactsFrequency currentTime
-    let startGameTact = GameTact 0 currentTime 0 0 currentTime nextTactTime 60.0
+    putStrLn "Loading..."
     
-    let gw = GameWorld homePoint (10, 4, 6)
-    let gameLoopF = gameLoop wnd mvars startGameTact
-    gameThreadId <- C.forkOS (gameThreadFunc gameLoopF gw)
-
-    startGlutLoop
---    C.killThread gameThreadId
-    putStrLn "Ok."
-
-
-
--- | Runs game thread function. Currently it is gameLoop in State monad with IO.
-gameThreadFunc :: GW () -> GameWorld -> IO ()
-gameThreadFunc = State.evalStateT
-
--- | Prints fps in console.
-showFps :: Float -> GW ()
-showFps fps = State.liftIO . putStrLn $ ("FPS: " ++ show fps)
-
--- | Prints information about elapsed time since previous frame.
-showElapsed :: Elapsed -> GW ()
-showElapsed (Elapsed ticks prevTime curTime elapsedTime) = State.liftIO $
-    putStrLn ("Ticks: " ++ show ticks ++ " PrevTime: " ++ show prevTime
-    ++ " CurTime: " ++ show curTime ++ " ElapsedTime: " ++ show elapsedTime)
-
--- Redraws GL window and puts message about frame to console.
-redrawFrame :: GLUT.Window -> GW ()
-redrawFrame wnd = State.liftIO . redrawGLWindow $ wnd
-
-recalculateWorld :: Elapsed -> GW ()
-recalculateWorld (Elapsed _ _ _ elapsedTime) = do
-    gw@(GameWorld (WorldDim ax ay az) (vx, vy, vz)) <- State.get
-    let xGrid = fromWorldAxis ax :: GalaxyGrid
-    let yGrid = fromWorldAxis ay :: GalaxyGrid
-    let zGrid = fromWorldAxis az :: GalaxyGrid
-    State.liftIO . putStrLn $ ("ObjPos: \n" ++ show xGrid ++ "\n" ++ show yGrid ++ "\n" ++ show zGrid)
+    W.boot W.workflowManager
     
-
--- | Main game loop. Processes incoming MVar (worldVar) and sends outcoming MVar (gameMVar).
-gameLoop :: GLUT.Window -> InteropMsg -> GameTact -> GW ()
-gameLoop wnd mvars@(gameMVar, worldMVar) gameTact = do
-
-    curTickTime <- State.liftIO getIOTickTime
-    let (isNewFrame, elapsed, newGameTact) = evalGameTact fpsSettings gameTact curTickTime
     
-    when isNewFrame (showFps (gtFps newGameTact) >> recalculateWorld elapsed >> redrawFrame wnd)
     
-    gameLoop wnd mvars newGameTact
-
-
-    
+    putStrLn "All Ok."
