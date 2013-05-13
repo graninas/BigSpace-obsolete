956279563654885ef1191fe227f0d4cbcd0dee99
Alexandr Granin <graninas@gmail.com>
2013-05-13 21:47:02 +0700

    - Status commit.

---
+++ CurvedSpace/CurvedSpace/Workflow/Logger.hs
@@ -0,0 +1,25 @@
+module Workflow.Logger (
+    err,
+    inf,
+    inf1,
+    log
+  ) where
+
+import System.Time
+import System.Locale
+import Text.Printf
+import Prelude hiding (log)
+
+type Formatter = (ClockTime, String) -> String -> String
+err, inf :: Formatter
+err (_, t) = printf "%s ERR %s" t
+inf (_, t) = printf "%s INF %s" t
+inf1 _ = printf "INF %s"
+
+log :: Formatter -> String -> IO ()
+log formatter msg = do
+    clockTime <- getClockTime
+    calTime <- toCalendarTime clockTime
+    let entryTime = formatCalendarTime defaultTimeLocale "[%d.%m.%Y %H:%M:%S]" calTime
+    let formattedMessage = formatter (clockTime, entryTime) msg
+    putStrLn formattedMessage
--- CurvedSpace/CurvedSpace/Workflow/WorkflowFacade.hs
+++ CurvedSpace/CurvedSpace/Workflow/WorkflowFacade.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Arrows, ExistentialQuantification #-}
+{-# LANGUAGE Arrows, ExistentialQuantification, FunctionalDependencies #-}
 
 module Workflow.WorkflowFacade where
 
@@ -9,90 +9,25 @@ import Control.Monad
 import qualified Control.Monad.IO.Class
 import qualified Control.Monad.State as State
 import qualified Data.Map as Map
-import System.Time
-import System.Locale
 
 import Prelude hiding (log, lookup, id, (.))
-import Text.Printf
-
-import qualified Control.Monad.Named as Named
 
 import Control.Arrow
 import Control.Category
 
-
-
-
-
-type WorkflowName = String
-type ItemKey = String
-
-data ContextItem = Logger (String -> IO ())
-                 | StringConst String               
-                 | forall m. (Monad m) => WorkflowList (WorkflowList String (m ()))
-
-type ContextItems = Map.Map ItemKey ContextItem
-data Context = Context 
-             { contextItems :: ContextItems
-             , parentContext :: Maybe Context
-             }
-
-
+import qualified Control.Monad.Named as Named
+import Workflow.Logger
 
 newtype Workflow a b = Workflow { runWorkflow :: a -> b }
-type WorkflowList a b = Map.Map WorkflowName (Workflow a b)
+type Inpure a b = Kleisli IO a b
 
-instance Arrow Workflow where
-  arr = Workflow
-  first (Workflow f) = Workflow (mapFst f)
-    where mapFst g (a,b) = (g a, b)
-  second (Workflow f) = Workflow (mapSnd f)
-    where mapSnd g (a, b) = (a, g b)
+
+
+wf1 = proc wf -> do
+    Kleisli (log inf) -< "Start workflow " ++ wf
+    Kleisli (log inf) -< "Done!!!"
     
-    
-instance Category Workflow where
-    (Workflow f) . (Workflow g) = Workflow (f . g)
-    id = arr id
-  
+wf2 = proc _ -> log inf -< "Here we go!" 
 
-
-type Formatter = (ClockTime, String) -> String -> String
-err, inf :: Formatter
-err (_, t) = printf "%s ERR %s" t
-inf (_, t) = printf "%s INF %s" t
-sinf _ = printf "INF %s"
-
-log :: Formatter -> Workflow String ( IO () )
-log formatter = proc msg -> do
---    (Logger logger) <- get "" "Logger"
---    clockTime <- liftIO getClockTime
---    calTime <- liftIO (toCalendarTime clockTime)
---    let entryTime = formatCalendarTime defaultTimeLocale "[%d.%m.%Y %H:%M:%S]" calTime
---    let formattedMessage = formatter (clockTime, entryTime) msg
---    liftIO $ logger formattedMessage
-    returnA -< putStrLn msg
-
-
-start wfName _ _ = runWorkflow wf1 wfName 
-
-
-wf1 :: Workflow String (IO ())
-wf1 = proc a -> do
-    res1 <- log inf -< ("Start workflow " ++ a)
-    --exec a emptyContext
-    res2 <- log inf -< "Done!!."
-    returnA -< (res2 >> res1)
-
-wf2 :: Workflow String (IO ())
-wf2 = proc _ -> log inf -< "Here we go!"
-
-wfList :: WorkflowList String (IO ())
-wfList = Map.fromList [("WF1", wf1), ("WF2", wf2)]
-
-
-boot startWF wfList = let
-    initialContext = Map.fromList [("FilePath", StringConst "/etc/xxx"), ("Logger", Logger putStrLn)]
-    in do
-    res <- start startWF wfList initialContext
-    putStrLn "AAA"
+boot = runKleisli wf1 "WF1"
 
